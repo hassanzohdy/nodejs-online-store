dcdbb56b7728fbb9cd86a8eac4e0f1bad0d7daf2
Hasan Zohdy <hassanzohdy@gmail.com>
2022-02-05 19:41:13 +0200

    Updating Base url

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,5 +1,18 @@
 import auth from "core/auth/middleware/auth";
 import guarded from "core/auth/middleware/guarded";
+import { log } from "core/log";
 import middlewareList from "core/router/middleware";
+import { NextFunction, Request, Response } from "express";
 
-middlewareList.add([auth]);
+function logRequest(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): any {
+  log(request.url);
+  log(request.baseUrl);
+
+  next();
+}
+
+middlewareList.add([logRequest, auth]);
