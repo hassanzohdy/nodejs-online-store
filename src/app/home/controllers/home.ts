ceed57bb21621c5005e0343f13ebf6cfde240ddf
Hasan Zohdy <hassanzohdy@gmail.com>
2022-02-07 10:52:51 +0200

    Added count method

--- src/app/home/controllers/home.ts
+++ src/app/home/controllers/home.ts
@@ -10,14 +10,7 @@ export default async function HomeController(
   request: Request,
   response: Response
 ) {
-  const data = await User._.where("id", ">", 15000).paginate({
-    size: 20,
-    page: 2,
-  });
-
-  data.records = data.records.map((user) => user.sharedData);
-
   response.send({
-    records: data,
+    records: await User.count(),
   });
 }
--- src/app/index.ts
+++ src/app/index.ts
@@ -3,14 +3,3 @@ import "./home";
 import "./customers";
 import "./products";
 import "./seeds";
-import database from "core/db";
-import User from "./models/User";
-import { log } from "core/log";
-
-database.on("connection", () => {
-  async function s() {
-    log(await User.count());
-  }
-
-  s();
-});
