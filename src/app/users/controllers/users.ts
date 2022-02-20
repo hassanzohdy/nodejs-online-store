09cc23bd5415bd6c4a8f4a06fa282c6903c16785
Hasan Zohdy <hassanzohdy@gmail.com>
2022-02-20 17:36:36 +0200

    Testing speed

--- src/app/users/controllers/users.ts
+++ src/app/users/controllers/users.ts
@@ -12,9 +12,10 @@ export default async function users(request: Request, response: Response) {
   });
 
   return response.success({
-    records: users.records.map((user) =>
-      user.only("id", "name", "email", "image")
-    ),
+    // records: users.records.map((user) =>
+    //   user.only("id", "name", "email", "image")
+    // ),
+    records: [],
     paginationInfo: users.info,
   });
 }
